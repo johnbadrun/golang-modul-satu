72ca08cce8003f43f34689aa49a4dd43e0285ea7
r1zq1 <rizqi.ardiansyah@gmail.com>
2023-03-07 00:27:04 +0700

    update say hello

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package golang_modul_satu
 
 func SayHelloFromGolangModulSatu(name string) string {
-	return "Hello " + name + " from golang modul satu"
+	return "Hello again " + name + " from golang modul satu"
 }
